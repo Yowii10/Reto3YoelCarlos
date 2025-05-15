b133a8293ffc050c60c6a55fb7a87982865fc9e2
Yowii10 <yoeliranzo@gmail.com>
2025-05-15 09:42:14 +0200

    Terminada clase clientesDao y productosDao

--- Reto3YoelCarlos/src/ClasesDAO/ClientesDAO.java
+++ Reto3YoelCarlos/src/ClasesDAO/ClientesDAO.java
@@ -5,10 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
 import Util.Conexion;
-import Util.Funciones;
 import Clases.Clientes;
 
 public class ClientesDAO {
@@ -33,4 +30,57 @@ public class ClientesDAO {
 	}
 	return cliente;
 }
+	public static List<Clientes> obtenerClientes() {
+        List<Clientes> clientes = new ArrayList<>();
+        try (Connection con = Conexion.abreConexion()) {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM clientes");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                clientes.add(new Clientes(
+                	rs.getInt("codigo"),     
+                 	rs.getInt("idcliente"),                
+                    rs.getString("direccion"),
+                    rs.getString("nombre")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return clientes;
+    }
+
+    public static Clientes buscarPorCodigo(int codigo) {
+        Clientes cliente = null;
+        try (Connection con = Conexion.abreConexion()) {  
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM clientes WHERE codigo = ?");
+            ps.setInt(1, codigo);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                cliente = new Clientes(
+                	rs.getInt("codigo"),  
+                  	rs.getInt("idcliente"), 
+                    rs.getString("direccion"),
+                    rs.getString("nombre")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cliente;
+    }
+
+    public static boolean insertarCliente(Clientes cliente) {
+        try (Connection con = Conexion.abreConexion()) {
+            PreparedStatement ps = con.prepareStatement("INSERT INTO clientes (nombre, direccion, codigo) VALUES (?, ?, ?)");
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getDireccion());
+            ps.setInt(3, cliente.getCodCliente());
+            return ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
